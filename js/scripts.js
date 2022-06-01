aaed573abd6f2b5eb9928e4cd1ab251510891805
Peter Vang <vang_peter06@yahoo.com>
2022-06-01 11:38:59 -0700

    Added dark and light button to HTML, style the background CSS, and fuction to button on JS.
    Co-authored-by: Sandra Tena <sandratena06@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,14 @@ $(document).ready(function() {
     $("#initially-showing").fadeIn();
     $("#initially-hidden").fadeOut();
   });
+
+$("button#black").click(function() {
+  $("body").removeClass();
+  $("body").addClass("black-background");
+});
+
+$("button#white").click(function() {
+  $("body").removeClass();
+  $("body").addClass("white-background");
+});
 });
